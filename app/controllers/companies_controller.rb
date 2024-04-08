e493450a9c28467aff8fb4522723ab9b2b4d0629
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-04-08 17:05:45 -0300

    refactored views

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,14 +1,28 @@
 class CompaniesController < ApplicationController
   def new
-    @companies = current_user.build_company.new
+    @company = Company.new
   end
 
   def edit
   end
 
   def create
+    @company = Company.new(company_params)
+    if @company.save
+      flash[:notice] = "Company successfully registered."
+      redirect_to root_path
+    else
+      flash[:error] = "Error when registering company."
+      render :new
+    end
   end
 
   def update
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name,:url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'applicants/index'
-  get 'applicants/new'
-  get 'positions/index'
-  get 'positions/new'
-  get 'positions/edit'
-  get 'positions/show'
-  resources :companies, only:[:new, :edit, :create, :update]
+  resources :companies, only: [:new, :edit, :create, :update]
   resources :positions
-  resources :applicants, only:[:new, :index, :create]
+  resources :applicants, only: [:new, :index, :create]
   root "home#index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
