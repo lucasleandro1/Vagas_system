311745c58c7058c327a90a13caaa89dca301659f
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-04-13 15:23:06 -0300

    add style in screen company

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :applicants, only: [:new, :index, :create]
   root to: "home#index"
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
